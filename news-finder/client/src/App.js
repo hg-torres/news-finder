fb5c2717f1977df692f045688c5b2a3d958a5a80
wearehavingfun <tim.bassignana@gmail.com>
2022-01-25 14:17:47 -0800

    updated packages

--- news-finder/client/src/App.js
+++ news-finder/client/src/App.js
@@ -2,11 +2,7 @@ import Appbar from './components/Appbar'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Appbar />
-=======
     <h1>Hello World!</h1>
->>>>>>> d78dd870efc240621971fad88ef21710b1d71402
   );
 }
 
