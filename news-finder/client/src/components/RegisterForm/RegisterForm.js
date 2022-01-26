af399d992d9c8347d1626d31683f763a97b6de34
HenryT <htorres572@gmail.com>
2022-01-26 12:05:01 -0800

    removed unnecessary imports

--- news-finder/client/src/components/RegisterForm/RegisterForm.js
+++ news-finder/client/src/components/RegisterForm/RegisterForm.js
@@ -3,7 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
@@ -98,13 +97,6 @@ const RegisterForm = () => {
             >
               Sign Up
             </Button>
-            {/* <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid> */}
           </Box>
         </Box>
       </Container>
