37e797ebe793f5529a280acd68fc9a64a7f157fe
bradleyjosephh <bradleyjosephh@gmail.com>
2022-01-28 14:16:00 -0800

    api calling

--- news-finder/client/src/App.js
+++ news-finder/client/src/App.js
@@ -12,18 +12,18 @@ import React, { useState, useEffect } from 'react'
 const App = () => {
 
   const [news, setNews] = useState([])
-  const [term, setTerm] = useState('politics')
+  const [categoryState, setCategoryState] = useState('')
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
         const res = await fetch(
-          `https://newsdata.io/api/1/news?apikey=${process.env.REACT_APP_NEWS_API_KEY}&category=${term}`
+          `https://newsdata.io/api/1/news?apikey=pub_4031193b8e4e6a29a91c4ed164279d46890f&country=${countryState}&language=${languageState}&category=${categoryState}`
         )
         const news = await res.json()
         console.log(news)
-        setNews(news.results.title)
+        setNews(news.results)
       } catch (error) {
         console.log(error);
       }
@@ -38,7 +38,13 @@ const App = () => {
         <Route path='/login' element={<Auth />} />
         <Route path='/home' element={<Home />} />
       </Routes>
+      {/* <section>
+        {news.map((news) => {
+          const {title, }
+        })}
+      </section> */}
     </Router>
+  
   )
 }
 
