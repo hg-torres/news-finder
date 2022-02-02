06186d008ac49ac0af5d163968e324d46ce2d91a
Loganuwu <53189601+Loganuwu@users.noreply.github.com>
2022-02-02 10:16:30 -0800

    updated Home Picture

--- news-finder/client/src/App.js
+++ news-finder/client/src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <Appbar />
-      {/* <Home /> */}
-      <Single />
+      <Home />
+      {/* <Single /> */}
 
 
     </>
--- news-finder/client/src/components/Header/Header.jsx
+++ news-finder/client/src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ function Header() {
   return <div className="header">
   <div className="headerTitles">
       {/* <span className="headerTitlesSm">News-Finder</span> */}
-      <span className="headerTitlesLg">News-Finder</span>
+      {/* <span className="headerTitlesLg">News-Finder</span> */}
   </div>
-  <img className="headerImg"src={require('./news-finder-logos.jpeg')} />
+  <img className="headerImg"src={require('./NewsShadow.png')} />
   </div>;
 }
 
