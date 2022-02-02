b5e9225a38d92b956506fd6877df7711c5b6b70a
Loganuwu <53189601+Loganuwu@users.noreply.github.com>
2022-02-02 11:43:08 -0800

    updated router

--- news-finder/client/src/App.js
+++ news-finder/client/src/App.js
@@ -1,16 +1,28 @@
 import Appbar from './components/Appbar'
 import Home from './pages/Home/Home';
 import Single from './pages/Home/single/Single';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
+import react from 'react';
 
 function App() {
   return (
-    <>
+    <Router>
       <Appbar />
+        <Switch>
+
+        </Switch>
+
       <Home />
       {/* <Single /> */}
 
 
-    </>
+    </Router>
     
   );
 }
