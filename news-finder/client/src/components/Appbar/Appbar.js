3a84bc010ae8a8d9218269fc58e4673e15251e5d
bradleyjosephh <bradleyjosephh@gmail.com>
2022-02-02 21:36:53 -0800

    profile js created and working on appbar links

--- news-finder/client/src/components/Appbar/Appbar.js
+++ news-finder/client/src/components/Appbar/Appbar.js
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Link from '@mui/material/Link'
 
 export default function ButtonAppBar() {
   return (
@@ -24,7 +25,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News Finder
           </Typography>
-          <Button  color="inherit">Login</Button>
+          <Button color="inherit">Login</Button>
           <Button color="inherit">Home</Button>
           <Button color="inherit">Profile</Button>
         </Toolbar>
