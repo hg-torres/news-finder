b2e124fdcc6ff1fc82b915cc86fd67f83a76918f
HenryT <htorres572@gmail.com>
2022-02-03 05:57:33 -0800

    reverted port changes

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.use(require('./routes'))
 app.get('*', (req, res) => res.sendFile(join(__dirname, 'client', 'build', 'index.html')))
 
 require('./db')
-  .then(() => app.listen(process.env.PORT || 3000)
+  .then(() => app.listen(process.env.PORT || 3001)
   .catch(err => console.log(err))
